9758983b5a58eb0d1c8f268ed27aa3788062428d
Jack <jackkershaw@protonmail.com>
2024-08-04 14:25:14 +0100

    Update jobboards.jsx

--- src/jobboards.jsx
+++ src/jobboards.jsx
@@ -8,24 +8,15 @@ const jobsites = [
     website: "https://fourdayweek.co.uk/find-a-job/",
   },
   { name: "NHS Jobs", website: "https://beta.jobs.nhs.uk/candidate" },
-  {
-    name: "London IT Jobs",
-    website: "https://findajob.dwp.gov.uk/search?cat=14&loc=86384",
-  },
   { name: "Grad Jobs", website: "https://giveagradago.com/" },
   {
     name: "The Dots",
-    website:
-      "https://the-dots.com/jobs/search?query=Web%20Developer&filters=job-type:Full%20Time.1|level:Junior.1^Mid%20Level.2^Senior.3|location:London%2C%20United%20Kingdom.70951&sortBy=latest",
-  },
-  {
-    name: "Work in Startups",
-    website: "https://workinstartups.com/job-board/jobs/developers",
+    website: "https://the-dots.com/jobs/search?",
   },
   {
     name: "Adzuna",
     website:
-      "https://www.adzuna.co.uk/jobs/value-my-cv/v3/job-ads?id=5296798&goal=front-end-developer",
+      "https://www.adzuna.co.uk/jobs/",
   },
   {
     name: "Arts Jobs",
@@ -35,12 +26,7 @@ const jobsites = [
   {
     name: "University Jobs",
     website:
-      "https://www.jobs.ac.uk/search/?activeFacet=nonAcademicDisciplineFacet&sortOrder=1&pageSize=25&startIndex=1&nonAcademicDisciplineFacet%5B0%5D=web-design-and-development",
-  },
-  {
-    name: "Total Jobs",
-    website:
-      "https://www.totaljobs.com/jobs/frontend-developer/in-london?radius=20&searchOrigin=Resultlist_top-search",
+      "https://www.jobs.ac.uk/search/",
   },
   { name: "Reed", website: "https://www.reed.co.uk/" },
   { name: "Monster", website: "https://www.monster.co.uk/" },
@@ -58,7 +44,6 @@ const jobsites = [
     website:
       "https://www.findapprenticeship.service.gov.uk/apprenticeshipsearch",
   },
-  { name: "CW Jobs", website: "https://www.cwjobs.co.uk/" },
   {
     name: "CV Library",
     website: "https://www.cv-library.co.uk/candidate/my-dashboard",
@@ -71,46 +56,14 @@ const jobsites = [
   {
     name: "Escape The City",
     website:
-      "https://www.escapethecity.org/search/jobs?q=option-listing-type%253DJob%2526option-seniority%253DJunior%25C2%25B7Entry-level%2526query%253Ddeveloper",
+      "https://www.escapethecity.org/search/jobs?
   },
-  { name: "F6s", website: "https://www.f6s.com/jobs" },
   {
     name: "Landing Jobs",
     website:
       "https://landing.jobs/jobs?location=London%2C+United+Kingdom+of+Great+Britain+and+Northern+Ireland&city=London&lr=50&hd=false&t_co=false&t_st=false",
   },
-  { name: "Wellfound", website: "https://wellfound.com/jobs" },
 
-  {
-    name: "Hired",
-    website: "https://hired.co.uk/",
-  },
-  { name: "Otta", website: "https://app.otta.com/jobs/" },
-  {
-    name: "Jobserve",
-    website: "https://www.jobserve.com/gb/en/Job-Search/",
-  },
-  { name: "Upwork", website: "https://www.upwork.com/" },
-  {
-    name: "Dice",
-    website:
-      "https://www.dice.com/jobs?q=frontend&location=London,%20UK&latitude=51.5072178&longitude=-0.1275862&countryCode=GB&locationPrecision=City&radius=30&radiusUnit=mi&page=1&pageSize=20&language=en",
-  },
-  {
-    name: "JS Jobs",
-    website:
-      "https://jsjobbs.com/jobs?country=uk&role=frontend%20developer",
-  },
-  {
-    name: "Power To Fly",
-    website:
-      "https://powertofly.com/jobs/?keywords=%22Frontend+Developer%22&location=London%2C+UK",
-  },
-  {
-    name: "Searchability",
-    website:
-      "https://searchability.com/tech-jobs/?_job_country=united-kingdom",
-  },
   {
     name: "GetMyFirstJob",
     website: "https://www.getmyfirstjob.co.uk/",
@@ -126,10 +79,6 @@ const jobsites = [
   {
     name: "If You Could",
     website: "https://www.ifyoucouldjobs.com/",
-  },
-  {
-    name: "Antal Tech Jobs",
-    website: "https://antaltechjobs.com/",
   },
 ];
 
